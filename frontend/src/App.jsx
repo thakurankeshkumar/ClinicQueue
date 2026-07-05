c3ce17d106fd944b0a56ec2acef0474982102374
Ankesh Kumar Thakur <thakurankeshkumar9@gmail.com>
2026-07-05 11:14:29 +0530

    Adding all dashboards to the app file to connect the path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PatientDashboard from "./pages/patient/Dashboard";
+import DoctorDashboard from "./pages/doctor/Dashboard";
+import AdminDashboard from "./pages/admin/Dashboard";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/patient/dashboard" element={<PatientDashboard />} />
+      <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
     </Routes>
   );
 }
